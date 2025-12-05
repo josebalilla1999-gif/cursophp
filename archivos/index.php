ea6c91c8d156c85af9aabef297eb30937584ee47
josebalilla1999-gif <josebalilla1999@gmail.com>
2025-12-05 13:24:03 +0100

    5-Dic-25 v1.1

--- archivos/index.php
+++ archivos/index.php
@@ -3,7 +3,14 @@
     $recursos = "static/";
     $carpeta = "templates/";
     $mensaje = '';
-    $extPermitidas = ['jpg', 'png', 'jpeg', 'doc', 'docx', 'pdf', 'txt', 'xlsx'];
+    $extPermitidas = ['jpg' => '#9800beff', 
+                        'png' => '#3f0037ff', 
+                        'jpeg' => '#e270ffff', 
+                        'doc' => '#0072beff', 
+                        'docx' => '#0d00beff', 
+                        'pdf' => '#ff0000ff', 
+                        'txt' => '#000000ff', 
+                        'xlsx' => '#00bebeff'];
     if(!is_dir($carpeta)){
         $mensaje = "El directorio no existe";
         mkdir($carpeta, 0755, true);
@@ -14,7 +21,7 @@
     if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['archivo'])){
         $nombre = basename($_FILES['archivo']['name']);
         $ext = strtolower(pathinfo($nombre, PATHINFO_EXTENSION));
-        if(in_array($ext, $extPermitidas)){
+        if(in_array($ext, array_keys($extPermitidas))){
             move_uploaded_file($_FILES['archivo']['tmp_name'], $carpeta.$nombre);
             $mensaje = 'Archivo permitido y subido';
         }else{
@@ -40,23 +47,10 @@
         <h1>Gestor de archivos</h1>
     </header>
     <main>
-        <section id="subida">
-            <form action="index.php" method="POST" enctype="multipart/form-data">
-                <label for="archivo">Selecciona un archivo: 
-                    <input type="file" id="archivo" name="archivo" accept="<?php
-                            foreach($extPermitidas as $ext){
-                                echo '.' . $ext . ',';
-                            }
-                        ?>application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document">
-                </label>
-                <button>Subir archivo</button>
-            </form>
-        </section>
-        <p><?= $mensaje ?></p>
         <table id="documentos">
             <thead>
                 <tr>
-                    <th>Nombre del archivo</th>
+                    <th colspan="2">Nombre del archivo</th>
                     <th>Tamaño</th>
                     <th>Ultima modificacion</th>
                     <th>Descargar</th>
@@ -64,21 +58,46 @@
             </thead>
             <tbody>
             <?php
-                foreach($archivos as $archivo){
-                    echo '<tr>';
-                    echo '<td><img src="static/img/file.svg">' . $archivo. '</td>';
-                    echo '<td>' . @filesize($carpeta.$archivo) .' bytes</td>';
-                    echo '<td>'. date('d-m-Y H:i:s', filemtime($carpeta.$archivo)+3600) .'</td>';
-                    echo '<td><a href="'. $carpeta.$archivo . '" download>Descargar fichero</a></td>';
-                    echo '</tr>';
+                foreach($archivos as $archivo){ 
+                    $ext = strtolower(pathinfo($archivo, PATHINFO_EXTENSION));
+                    $color = $extPermitidas[$ext];
+                ?>
+                    <tr>
+                    <td><figure><img src="static/img/file.svg" alt="Extension del tipo<?= $ext; ?>"><figcaption style="color: <?= $color; ?>"><?= $ext;?></figure></td>
+                    <td><?= $archivo; ?></td>
+                    <td><?= @filesize($carpeta.$archivo)?> bytes</td>
+                    <td><?=date('d-m-Y H:i:s', filemtime($carpeta.$archivo)+3600);?></td>
+                    <td><a href="<?=$carpeta.$archivo;?>" download>Descargar fichero</a></td>
+                    </tr><?php
                 }
-            ?></tbody>
+            ?>
+            </tbody>
         </table>
-
+        <section id="subida">
+            <form action="index.php" method="POST" enctype="multipart/form-data">
+                <label for="archivo">Selecciona un archivo: 
+                    <input type="file" id="archivo" name="archivo" accept="<?php
+                            foreach(array_keys($extPermitidas) as $ext){
+                                echo '.' . $ext . ',';
+                            }
+                        ?>">
+                </label>
+                <label for="drag">
+                    <input type="file" id="drag" name="drag" placeholder="Arrastra un archivo aqui" accept="<?php
+                            foreach(array_keys($extPermitidas) as $ext){
+                                echo '.' . $ext . ',';
+                            }
+                        ?>">
+                </label>
+                <button>Subir archivo</button>
+            </form>
+        </section>
+        <p><?= $mensaje ?></p>
     </main>
     <footer>
         <p>Hecho por er Chemita, con su gracia y su arte. Ole!</p>
     </footer>
+    <script src="<?= $recursos?>js/script.js"></script>
     <script src="<?= $recursos?>js/highlight.min.js"></script>
     <script>hljs.highlightAll();</script>
 </body>
---
+++ archivos/static/js/script.js
@@ -0,0 +1,16 @@
+const zona = document.getElementById("drag");
+zona.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    zona.classList.add('drag');
+});
+zona.addEventListener("dragleave", (e) => {
+    e.preventDefault();
+    zona.classList.remove('drag');
+});
+zona.addEventListener("drop", (e) => {
+    e.preventDefault();
+    zona.classList.remove('drag');
+    alert('Ojo cuidao, acabas de soltar un archivo en la pagina');
+    let archivo = e.dataTransfer.files[0];
+    console.log(archivo.name);
+});
